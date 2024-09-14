2c63125f4e6974a6897e654acb084893a0ebf000
shubh-ummm <shubhamn2717@gmail.com>
2024-09-14 13:03:57 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-sf-pro font-bold text-4xl">This is SF Pro Bold</h1>
+      <p className="font-sf-pro font-light text-base">This is SF Pro Light</p>
+      <p className="font-sf-pro font-normal text-base">
+        This is SF Pro Regular
+      </p>
+      <p className="font-sf-pro font-medium text-base">This is SF Pro Medium</p>
+    </main>
+  );
+}
